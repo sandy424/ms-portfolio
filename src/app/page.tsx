c63983be0bf56833ff1055be2f455cc1e2fcacb1
sandy424 <sandy424@naver.com>
2026-09-02 11:16:40 +0900

    Feat: AboutMe 소개글 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Header from "../components/Header";
 import Intro from "../components/Intro";
+import AboutMe from "../components/AboutMe";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col px-5 py-10 sm:px-8 sm:py-14">
-      <div className="mx-auto w-full max-w-2xl">
+    <main className="h-screen overflow-y-auto snap-y snap-mandatory scroll-smooth">
+      <div className="mx-auto w-full max-w-2xl px-5 pt-10 sm:px-8 sm:pt-14">
         <Header />
       </div>
-      <Intro />
+      <div className="flex flex-1">
+        <Intro />
+      </div>
+      <AboutMe />
     </main>
   );
 }
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,14 +2,14 @@ import TerminalTyping from "./TerminalTyping";
 
 export default function Intro() {
   return (
-    <section className="flex w-full flex-1 items-center justify-center py-4 sm:py-8">
-      <div className="mx-auto flex w-full max-w-[1600px] flex-col items-center justify-center gap-8 xl:flex-row xl:justify-between px-18">
+    <section className="flex min-h-screen w-full items-center justify-center px-5 py-10 sm:px-8 sm:py-14">
+      <div className="mx-auto flex w-full max-w-[1600px] flex-col items-center justify-center gap-8 px-18 xl:flex-row xl:justify-between">
         <div className="flex flex-col gap-4 xl:max-w-[46rem]">
-          <h1 className="mb-4 text-3xl font-bold leading-snug tracking-tight sm:text-4xl">
+          <h1 className="mb-4 text-5xl font-bold leading-snug tracking-tight">
             프론트엔드 개발자를 목표로 하는 <br />
             강민서입니다.
           </h1>
-          <p className="text-base font-medium leading-relaxed text-gray-600">
+          <p className="text-xl font-medium leading-relaxed text-gray-600">
             사용자가 쉽게 이해하고 편리하게 사용할 수 있는<br />
             웹서비스를 만드는 것을 좋아합니다.
           </p>
